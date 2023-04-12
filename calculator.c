7c2f1c94639397aa5f6a81a4bdfb5c1e1f133f80
zunky4u24 <odewaleolasunkanmi123@gmail.com>
2023-04-12 13:01:25 -0700

    calculator.c

---
+++ calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+/* this is a simple calculator
+ * its contains the code
+ */
+
+int main(void)
+{
+	int x = 2
+	int y = 5
+
+	int z = y + x
+
+	printf("%d", z);
+}
